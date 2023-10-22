12a3062c38cd1fba3e7b2b59567c8c86bd80f286
Torres <gustavo.souza@sptech.school>
2023-10-22 01:07:52 -0300

    novo jar

---
+++ data-sync2/src/main/java/sistemaCaptura/telasSistema/AppHistorico.java
@@ -0,0 +1,12 @@
+package sistemaCaptura.telasSistema;
+
+import sistemaCaptura.HistConsmRecurso;
+
+public class AppHistorico {
+    public static void main(String[] args) {
+       HistConsmRecurso histConsmRecurso = new HistConsmRecurso();
+
+       histConsmRecurso.mostrarHistorico();
+
+    }
+}
